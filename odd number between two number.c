49c7f60bdb82a7815cca5ccd3bc40f4c5b19652a
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2018-12-13 22:06:32 +0530

    Add files via upload

---
+++ odd number between two number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2;
+    scanf("%d%d",&n1,&n2);
+    for(;n1<n2;n1++)
+    {
+        if(n1%2==1)
+            {printf("%d ",n1);
+            }
+    
+    }
+    return 0;
+}
