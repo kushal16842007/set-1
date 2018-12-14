705699d200c3acd31bebeeb7189158079ee826bf
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2018-12-15 01:47:30 +0530

    Add files via upload

---
+++ nextRound.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main() {
+   int n,t,byes=0,br=0;
+   scanf("%d",&n);
+   while(n!=1)
+   {
+   t=2;
+   while(t<n)
+   {
+       t = t*2;
+   }
+   t=t / 2;
+   n=n-t;
+   byes = byes + n;
+   }
+   printf("%d",byes);
+}
