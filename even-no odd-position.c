33c9cc03387b6647b364066a2e72777b8dab50c5
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2018-12-14 22:34:44 +0530

    Add files via upload

---
+++ even-no odd-position.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main() {
+   int n,f=0;
+   scanf("%d",&n);
+   int ar[n];
+   for(int i=0;i<n;i++)
+   {
+       scanf("%d",&ar[i]);
+   }
+   
+   for(int i=0;i<n;i++)
+   {
+       if(i%2 != ar[i]%2)
+       printf("%d ",ar[i]);
+   }
+}
