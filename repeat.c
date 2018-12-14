d6257ff6e36475c427976e5a904a7ef994a223f5
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2018-12-14 22:23:38 +0530

    Add files via upload

---
+++ repeat.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+
+int main() {
+   int n,f=0;
+   scanf("%d",&n);
+   int ar[n];
+   for(int i=0;i<n;i++)
+   {
+       scanf("%d",&ar[i]);
+   }
+   int t=ar[1];
+   for(int i=0;i<n;i++)
+   {
+       if(i==1)
+       continue;
+       if(t==ar[i])
+       f++;
+   }
+   if(f>0)
+   printf("%d",t);
+   else
+   printf("unique");
+}
