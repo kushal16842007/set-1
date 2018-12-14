7fc43cbb88067a2df030dbc5dd621b2d5db30dd5
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2018-12-14 16:34:39 +0530

    Add files via upload

---
+++ prime between two interval.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+int main()
+{
+    int s,e,j,f;
+    scanf("%d%d",&s,&e);
+    for(int i=s;i<=e;i++)
+    {
+        f=0;
+        j=2;
+        while(j<i)
+        {
+            if(i%j==0)
+            {
+                f=1;
+                break;
+            }
+            j++;
+            
+        }
+        if(f==0)
+        {
+            printf("%d ",i);
+        }
+    }
+   
+    return 0;
+}
