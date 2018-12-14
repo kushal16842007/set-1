71c544e2be1316c5a3cc7188c712ed0995f4b13f
kushal16842007 <36362403+kushal16842007@users.noreply.github.com>
2018-12-14 22:11:38 +0530

    Add files via upload

---
+++ appear only once.c
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+
+int main() {
+   int n;
+   scanf("%d",&n);
+   int ar[n];
+   for(int i=0;i<n;i++)
+   {
+       scanf("%d",&ar[i]);
+   }
+   for(int i=0;i<n;i++)
+   {
+       for(int j=i+1;j<n;j++)
+       {
+           if(ar[i]==ar[j])
+           {
+               ar[i]=ar[j]=0;
+               break;
+           }
+       }
+   }
+   for(int i=0;i<n;i++)
+   {
+       if(ar[i]!=0)
+       {
+           printf("%d",ar[i]);
+           break;
+       }
+   }
+}
